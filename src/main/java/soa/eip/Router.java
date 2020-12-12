ecbc4de838ff43deff9be815ada9e9beb913de0c
David Alloza Tejero <davidallozatejero@MacBook-Pro-de-David.local>
2020-12-12 11:09:57 +0100

    Extended query interface to support max:n command

--- src/main/java/soa/eip/Router.java
+++ src/main/java/soa/eip/Router.java
@@ -2,6 +2,8 @@ package soa.eip;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 public class Router extends RouteBuilder {
@@ -13,6 +15,25 @@ public class Router extends RouteBuilder {
     from(DIRECT_URI)
       .log("Body contains \"${body}\"")
       .log("Searching twitter for \"${body}\"!")
+      
+      // Interceptar mensaje antes de enviarlo a la API de Twitter
+      .process(exchange -> {
+          // Obtener contenido del mensaje
+          String body = exchange.getIn().getBody(String.class);
+          // Detectar número max mediante patrones.
+          Pattern regex = Pattern.compile("max:(\\d+)");
+          Matcher m = regex.matcher(body);
+          // Patrón detectado
+          if (m.find()) {
+              int maxNum = Integer.parseInt(m.group(1));
+              // Quitar del mensaje el string "max:N"
+              body = body.replace("max:" + maxNum,"").trim();
+              // Añadir como parámetro el número max.
+              body += "?count=" + maxNum;
+              // Actualizar contenido del mensaje
+              exchange.getOut().setBody(body);
+          }
+      })
       .toD("twitter-search:${body}")
       .log("Body now contains the response from twitter:\n${body}");
   }
